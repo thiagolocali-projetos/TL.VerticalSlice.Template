87e3285c403077073e561faca4733c79d747ff73
Thiago Locali <thiagolocali@hotmail.com>
2026-04-26 17:14:47 -0300

    ajustes rabbit

--- src/TL.Exemplo.Application/Features/Rabbit/ConsumirMensagem/ConsumirMensagemCommand.cs
+++ src/TL.Exemplo.Application/Features/Rabbit/ConsumirMensagem/ConsumirMensagemCommand.cs
@@ -2,18 +2,23 @@ using MediatR;
 using TL.Exemplo.Application.Common.Models;
 using TL.Exemplo.Application.Contracts.Messaging;
 
-namespace TL.Exemplo.Application.Features.Rabbit.ProduzirMensagem;
+namespace TL.Exemplo.Application.Features.Rabbit.ConsumirMensagem;
 
-public record ProduzirMensagemCommand(string Fila, string Conteudo) : IRequest<ApiResponse<object>>;
+public record ConsumirMensagemCommand(string Fila) : IRequest<ApiResponse<object>>;
 
-public class PublicarMensagemHandler : IRequestHandler<ProduzirMensagemCommand, ApiResponse<object>>
+public class ConsumirMensagemHandler : IRequestHandler<ConsumirMensagemCommand, ApiResponse<object>>
 {
     private readonly IRabbitMqService _rabbit;
-    public PublicarMensagemHandler(IRabbitMqService rabbit) => _rabbit = rabbit;
+    public ConsumirMensagemHandler(IRabbitMqService rabbit) => _rabbit = rabbit;
 
-    public async Task<ApiResponse<object>> Handle(ProduzirMensagemCommand request, CancellationToken ct)
+    public async Task<ApiResponse<object>> Handle(ConsumirMensagemCommand request, CancellationToken ct)
     {
-        await _rabbit.ProduzirAsync(request.Fila, new { request.Conteudo, Data = DateTime.UtcNow });
-        return ApiResponse<object>.Ok(new { request.Fila }, "Mensagem publicada");
+        await _rabbit.ConsumirAsync<dynamic>(request.Fila, async msg =>
+        {
+            // aqui você processa a mensagem (pode logar, salvar, etc.)
+            await Task.CompletedTask;
+        }, ct);
+
+        return ApiResponse<object>.Ok(new { request.Fila }, "Consumidor iniciado");
     }
 }
